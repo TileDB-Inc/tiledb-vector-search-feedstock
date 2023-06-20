2ae01d80f25b5a7919e0c868bfea32f3b1811559
Seth Shelnutt <seth@tiledb.io>
2023-06-20 10:30:51 -0400

    Cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
 set TILEDB_CONDA_BUILD=1
 
+set CMAKE_BUILD_TYPE=Release
+set DOWNLOAD_TILEDB_PREBUILT=OFF
+set BUILD_TESTS=OFF
+set USE_MKL_CBLAS=OFF
 cd apis\python
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -10,38 +10,9 @@ if [ ! -z ${MACOSX_DEPLOYMENT_TARGET}]; then
     export CMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}
 fi
 
-#CURL_LIBS_APPEND=`$PREFIX/bin/curl-config --libs`
-#export LDFLAGS="${LDFLAGS} ${CURL_LIBS_APPEND}"
-
-#if [[ $target_platform  == linux-64 ]]; then
-#  export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed -lrt"
-#fi
-#
-#if [[ $target_platform == osx-arm64  ]]; then
-#  export FILE_COMMAND_OVERRIDE=`which file`
-#fi
-
-#cd src
-#mkdir build && cd build
-#cmake ${CMAKE_ARGS} \
-#  -DOVERRIDE_INSTALL_PREFIX=OFF \
-#  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
-#  #-DCMAKE_INSTALL_PREFIX:PATH="../../api/python/src/tiledb/vector_search" \
-#  -DCMAKE_BUILD_TYPE=Release \
-#  -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
-#  -DDOWNLOAD_TILEDB_PREBUILT=OFF \
-#  -DBUILD_TESTS=OFF \
-#  -DUSE_MKL_CBLAS=OFF \
-#  ..
-#make -j ${CPU_COUNT}
-#make -C libtiledbvectorsearch install
-
-#PYTHONPATH=/tmp/build/libtiledbvectorsearch/python/src/tiledb/vector_search
 cd apis/python
-#$PYTHON setup.py install --single-version-externally-managed --record record.txt --libtiledbvectorsearch="${PREFIX}"
 export CMAKE_BUILD_TYPE=Release
 export DOWNLOAD_TILEDB_PREBUILT=OFF
 export BUILD_TESTS=OFF
 export USE_MKL_CBLAS=OFF
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
-#&& pip install -e .
